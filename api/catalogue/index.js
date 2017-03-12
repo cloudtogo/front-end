d46bfa48e366eb60ba1ee686aac69892083d1d1f
hongxima <homa.cn@gmail.com>
2017-03-12 03:39:00 +0000

    add put to the api route

--- api/catalogue/index.js
+++ api/catalogue/index.js
@@ -25,5 +25,24 @@
   app.get("/comment*", function (req, res, next) {
     helpers.simpleHttpRequest(endpoints.commentUrl + req.url.toString(), res, next);
   });
+
+  app.put("/comment*", function (req, res, next) {
+        var options = {
+            uri: endpoints.commentUrl + req.url.toString(),
+            method: 'PUT',
+            json: true,
+            body: req.body
+        };
+        console.log("Puting Comment: " + JSON.stringify(req.body));
+        request(options, function(error, response, body) {
+            if (error) {
+                return next(error);
+            }
+            helpers.respondSuccessBody(res, JSON.stringify(body));
+        }.bind({
+            res: res
+        }));
+    });
+
   module.exports = app;
 }());
